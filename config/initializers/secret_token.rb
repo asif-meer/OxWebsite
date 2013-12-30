8ce5e03b4e80a8897f77411311f03da31b7dfead
asif-meer <asif_meer_2k6@yahoo.com>
2013-12-30 15:35:10 +0500

    started project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OxWebsite::Application.config.secret_token = '1ad85143ad6ef743410d0cb9cb1b5cf93a642ae44bb499d6f8f43c104ddc9e7ed9ef56caf54361ff08d1e9190cc2265e84dcfdd0439ce3447e92148c3e55ec3f'
