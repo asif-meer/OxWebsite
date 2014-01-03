296566e6e9452c11c641abf2932c297ea772e60c
Sarmad-Sabih <asif_meer_2k6@yahoo.com>
2014-01-04 00:39:52 +0500

    Article creation logic

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,8 +9,29 @@ class ArticlesController < InheritedResources::Base
     @article=Article.find(params[:id])
   end
 
+  def create
+    @article = Article.new(params[:article])
+    if params[:subcategory].present?
+      @subcategory=Subcategory.find(params[:subcategory])
+      @article.container=@subcategory
+    else
+      @category=Category.find(params[:category])
+      @article.container=@category
+    end
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to @article, notice: 'Article was successfully created.' }
+        format.json { render json: @article, status: :created, location: @article }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def get_subcategories
-    @sub_categories= Subcategory.find(params[:category_id])
+    @category= Category.find(params[:category_id])
+    @subcategories=@category.subcategories
     respond_to do |format|
       format.js
     end
